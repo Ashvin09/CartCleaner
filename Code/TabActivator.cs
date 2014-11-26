a709945612746964f4b6c889a9d7c775d7156260
Ashvin09 <ashvingajbhiye09@gmail.com>
2014-11-26 18:08:46 +0530

    New

---
+++ Code/TabActivator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MozuViewHelper
+{
+    public static class TabActivator
+    {
+        public static string ActivePage(this HtmlHelper helper, string controller, string action)
+        {
+            string classValue = "";
+
+            string currentController = helper.ViewContext.Controller.ValueProvider.GetValue("controller").RawValue.ToString();
+            string currentAction = helper.ViewContext.Controller.ValueProvider.GetValue("action").RawValue.ToString();
+
+            if (currentController == controller && currentAction == action)
+            {
+                classValue = "selected";
+            }
+
+            return classValue;
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Mozu.Api.Resources.Platform;
+using Mozu.Api.WebToolKit.Filters;
+
+namespace CartCleaner.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[HttpPost]
+        //[ConfigurationAuthFilter]
+        public async Task<ActionResult> Index(int? tenantId)
+        {
+
+            if (tenantId.HasValue)
+            {
+                var tenantResource = new TenantResource();
+                var tenant = await tenantResource.GetTenantAsync(tenantId.Value);
+            }
+
+            string cookieToken;
+            string formToken;
+
+            AntiForgery.GetTokens(null, out cookieToken, out formToken);
+            ViewBag.cookieToken = cookieToken;
+            ViewBag.formToken = formToken;
+            return View();
+        }
+        public ActionResult ConfigRule()
+        {
+            return View();
+        }
+        public ActionResult ViewRule()
+        {
+            return View();
+        }
+        public ActionResult ViewOrder()
+        {
+            return View();
+        }
+        public ActionResult ViewHistory()
+        {
+            return View();
+        }
+    }
+}
